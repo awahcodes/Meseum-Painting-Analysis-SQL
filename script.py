d8a47f18d4d0cd8ed70a247b166b42989e3b50d0
awahcodes <awahcodes@gmail.com>
2024-12-09 17:50:15 +0000

    Did check that all datasets have imported to database
    Used query 'select * from table_name limit 10 to understand the dataset'

--- script.py
+++ script.py
@@ -1,6 +1,15 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+#connect to server
 conn_string = 'postgresql://postgres:Onajourney123#@localhost/museum'
 db = create_engine(conn_string)
 conn = db.connect()
+
+#import csv dataset into database 'museum' using loop
+
+files = ['artist', 'canvas_size', 'image_link', 'museum_hours', 'museum', 'product_size', 'subject', 'work']
+
+for file in files:
+    df = pd.read_csv(f'C:/Users/aniwa/OneDrive/Desktop/New World/Data Analyst Portfolio/Museum Painting Analysis in SQL/Museum_Dataset/{file}.csv')
+    df.to_sql(file, con=conn, if_exists='replace', index=False)
